ed49b56e102daf36d544f1232d3534731a560b51
Cuycr77 <143004840+Cuycr77@users.noreply.github.com>
2024-10-10 14:41:41 -0500

    pro

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import modelo.conexion;
-import vista.vista;
-
-public class Main {
-    public static void main(String[] args) {
-        vista view = new vista();
-        view.Conectar();
-    }
-}
